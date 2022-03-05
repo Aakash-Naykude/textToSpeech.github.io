48cafec0d4d3fe3d381e087340946a61ee367f46
Aakash-Naykude <aakashnaykude123@gmail.com>
2022-03-05 19:43:44 +0530

    completed

---
+++ index.js
@@ -0,0 +1,41 @@
+const playbtn = document.getElementById("playbtn");
+const pausebtn = document.getElementById("pausebtn");
+const stopbtn = document.getElementById("stopbtn");
+const textInput = document.getElementById("text");
+const speedInput = document.getElementById("speed");
+let currentCharacter;
+playbtn.addEventListener("click", () => {
+  playText(textInput.value);
+});
+stopbtn.addEventListener("click", stopText);
+pausebtn.addEventListener("click", pauseText);
+speedInput.addEventListener("input", () => {
+  stopText();
+  playText(utterrance.text.substring(currentCharacter));
+});
+const utterrance = new SpeechSynthesisUtterance(text);
+utterrance.addEventListener("end", () => {
+  textInput.disabled = false;
+});
+utterrance.addEventListener("boundary", (e) => {
+  currentCharacter = e.charIndex;
+});
+function playText(text) {
+  if (speechSynthesis.paused && speechSynthesis.speaking) {
+    return speechSynthesis.resume();
+  }
+  if (speechSynthesis.speaking) return;
+  utterrance.text = text;
+  utterrance.rate = speedInput.value || 1;
+  textInput.disabled = true;
+  speechSynthesis.speak(utterrance);
+}
+function pauseText() {
+  if (speechSynthesis.speaking) {
+    speechSynthesis.pause();
+  }
+}
+function stopText() {
+  speechSynthesis.resume();
+  speechSynthesis.cancel();
+}
